4d2f1e8aadc2c6813b49119c169cba2db1ebcbc9
TrTo <trto@strongtie.com>
2020-09-19 20:37:17 +0700

    init

---
+++ cypress/integration/demo/demoRP.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+
+
+
+describe('Demo cypress with report mochawesome', function() {
+    
+    
+    it('go to google', function() {
+        cy.visit("https://www.google.com.vn/")
+    });
+        
+});
+    
